c20204bb0f3a63dd7384bb4de935484ba8f3cca4
Tunnel1337 <>
2013-05-31 23:17:44 +0200

    Added a GPL license

--- resize.sh
+++ resize.sh
@@ -1,8 +1,24 @@
+#Copyright 2013 Jose Martinez Gonzalez (Tunnel1337)
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
 #!/bin/bash
 #
-# Resize and/or convert all files from a given directory to an output directory with a custom resize factor.
+#Resize and/or convert all files from a given directory to an output directory with a custom resize factor.
 #
-# Author: Jose Martinez Gonzalez (Tunnel1337)
+#Author: Jose Martinez Gonzalez (Tunnel1337)
 #
 
 inputFolder=$1
@@ -28,16 +44,16 @@ if [ $# -ne 4 ]
 	exit 1
 fi
 
-# Create output folder
+#Create output folder
 $(cp -r $inputFolder $outputFolder)
 
-# Find all files
+#Find all files
 data=$(find $outputFolder -name "*.png" && find $outputFolder -name "*.gif" && find $outputFolder -name "*.jpg")
 
-# Number of found files
+#Number of found files
 fileNumber=$(find $outputFolder -name "*.png" && find $outputFolder -name "*.gif" && find $outputFolder -name "*.jpg" | wc -w)
 
-# Loop over all files
+#Loop over all files
 for line in $data
 	do
 	tmpFile=$line
@@ -46,10 +62,10 @@ for line in $data
         origFormat=${tmpFile:$length-3:$length}
 
 	echo "Resizing ${tmpFile}"
-        # the actual resize method
+        #Actual resize method
 	$(convert $tmpFile -resize $factor% $file.$format)
 	
-        # Delete old unconverted file
+        #Delete old unconverted files
         if [ $origFormat != $format ]
             then $(rm $tmpFile)
         fi
